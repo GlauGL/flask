636aceefebcf69127898384fe896e69322543a3c
Glauter Bacellar <124213662+GlauGL@users.noreply.github.com>
2023-07-04 21:49:14 -0300

    Update calcula_raiz.py

--- calcula_raiz.py
+++ calcula_raiz.py
@@ -1,8 +1,8 @@
-numero = int(input('Digite um número: '))
-for i in range(numero):
-    if i * i == numero:
-        print(f'A raiz do numero {numero}={i}')
-        break
-    if i * i > numero:
-        print('Numero não existe')
-        break    
+def calcula_raiz(numero):
+    for i in range(numero):
+        if i * i == numero:
+            print(f'A raiz do numero {numero}={i}')
+            break
+        if i * i> numero:
+            print('Numero não existe')
+            break    
